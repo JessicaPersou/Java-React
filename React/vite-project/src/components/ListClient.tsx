75e06c4eedbbb864365e5d793e630508932c190d
JessicaPersou <jessica.sousa@koin.com.br>
2022-09-22 00:53:38 -0300

    add delete

--- React/vite-project/src/components/ListClient.tsx
+++ React/vite-project/src/components/ListClient.tsx
@@ -1,49 +1,54 @@
 import axios from "axios"
 import moment from "moment"
+import { useEffect, useState } from "react"
 import { IClient } from '../models/client'
-import { useState, useEffect} from "react"
+import { DeleteId } from "./DeleteClient"
 
 
-export function ListClient(){
+export function ListClient() {
 
-    const [list, setList] = useState<IClient[]>([])
+  const [list, setList] = useState<IClient[]>([])
 
-    useEffect(()=> { 
-  
-      axios("http://localhost:8080/client").then((response) => {
-  
-        setList(response.data)
-  
-      })
-  
-    },[])
+  useEffect(() => {
 
-    return(
-        <table>
-        <thead>
-          <tr>
-            <th>Nome</th>
-            <th>CPF</th>
-            <th>Data</th>
-            <th>Id</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            list.map(item =>
+    axios("http://localhost:8080/client").then((response) => {
+
+      setList(response.data)
+
+    })
+
+  }, [])
+
+
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>CPF</th>
+          <th>Data</th>
+          <th>Id</th>
+          <th>Deletar</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          list.map(item =>
 
             <tr key={item.id}>
               <td>{item.fullName}</td>
               <td>{item.cpf}</td>
               <td>{item.date && moment(item.date).format("DD/MM/YYYY")}</td>
-              <td>{item.id}</td>
-            </tr> 
+              <td id="delId">{item.id}</td>
+              <td><button onClick={() => {DeleteId}}>Apagar</button></td>
+            </tr>
 
           )
-          }
-          
-        </tbody>
-      </table>
-    )
+        }
+
+      </tbody>
+    </table>
+  )
 
 }
